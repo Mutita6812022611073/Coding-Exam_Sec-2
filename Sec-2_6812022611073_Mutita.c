8a9575e25584df02b9e2a63d86890f6631b68cca
Mutita6812022611073 <s6812022611073@email.kmutnb.ac.th>
2025-10-07 15:17:12 +0700

    Add files via upload

---
+++ Sec-2_6812022611073_Mutita.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    char score[5],i[5];      
+
+   printf("*** Transaction Fee Calculator***\n ");
+    scanf("%s", &score);
+
+    printf("Enter numbre of transactions (3-12): ");
+    scanf("%d", &score);
+    
+   printf("Enter Channel Type for TXN 1 (O,B,A): ");
+    scanf("%s", &score);
+
+
+   printf("Enter Channel Type for TXN 5 (O,B,A): ");
+    scanf("%s", &score);
+
+    printf("Enter Total Transaction Value: ");
+    scanf("%d", &score);
+ 
+
+    return 0;
+}
